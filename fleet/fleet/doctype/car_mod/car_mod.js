c3bc144ff4c5c3c2670a3facf1e4f4362f0e4a94
Gaurav Naik <gauravnaik6288@gmail.com>
2016-04-20 23:46:15 +0530

    added car mod dt

---
+++ fleet/fleet/doctype/car_mod/car_mod.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, Revant Nandgaonkar and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Car Mod', {
+	refresh: function(frm) {
+
+	}
+});
